cc630ce31474f73c7d2e2ad2d25b6d8519594929
uzeun <uzeunyi@gmail.com>
2023-03-26 06:50:39 +0900

    Add GET methods

--- src/todo.rs
+++ src/todo.rs
@@ -1,4 +1,4 @@
-use rocket::serde::json::{json, Json, Value};
+use rocket::serde::json::{json, Value};
 use rocket::serde::{Deserialize, Serialize};
 use rocket::tokio::sync::Mutex;
 use rocket::State;
@@ -29,14 +29,26 @@ async fn new(title: String, list: Todos<'_>) -> Value {
     json!({"status": "ok", "id": id})
 }
 
-// TODO: GET /todos
-// TODO: GET /todo/<id>
+#[get("/", format = "json")]
+async fn get_list(list: Todos<'_>) -> Value {
+    let list = list.lock().await;
+    json!(list.as_slice())
+}
+
+#[get("/<id>", format = "json")]
+async fn get(id: Id, list: Todos<'_>) -> Option<Value> {
+    let list = list.lock().await;
+    let todo = list.get(id)?;
+    Some(json!(todo))
+}
+
 // TODO: PUT /todo/<id>
+// TODO: DELETE /todo/<id>
 
 pub fn stage() -> rocket::fairing::AdHoc {
     rocket::fairing::AdHoc::on_ignite("TODO", |rocket| async {
         rocket
-            .mount("/todo", routes![new])
+            .mount("/todo", routes![new, get_list, get])
             .manage(TodoList::new(vec![]))
     })
 }
