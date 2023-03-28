427f5efaf68f3eca8cba61d72c4889a58a3fc44d
uzeun <uzeunyi@gmail.com>
2023-03-28 20:23:59 +0900

    Add PUT method

--- src/todo.rs
+++ src/todo.rs
@@ -42,13 +42,25 @@ async fn get(id: Id, list: Todos<'_>) -> Option<Value> {
     Some(json!(todo))
 }
 
-// TODO: PUT /todo/<id>
+#[put("/<id>", format = "json", data = "<title>")]
+async fn edit_title(id: Id, title: String, list: Todos<'_>) -> Option<Value> {
+    match list.lock().await.get_mut(id) {
+        Some(existing) => {
+            let title_ref = &mut existing.title;
+            *title_ref = title;
+            Some(json!({"status": "ok"}))
+        }
+        None => None,
+    }
+}
+
+// TODO: PUT /todo/<id>/complete
 // TODO: DELETE /todo/<id>
 
 pub fn stage() -> rocket::fairing::AdHoc {
     rocket::fairing::AdHoc::on_ignite("TODO", |rocket| async {
         rocket
-            .mount("/todo", routes![new, get_list, get])
+            .mount("/todo", routes![new, get_list, get, edit_title])
             .manage(TodoList::new(vec![]))
     })
 }
